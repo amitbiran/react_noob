1c5a117af8284b83a593c47b8bb2609f1a121fe7
amitbiran <amitbiran2@gmail.com>
2021-06-21 11:11:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>Eve's Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>I Am Groot!!!!</p>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>He Really Is Groot</p>
+    </footer>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}

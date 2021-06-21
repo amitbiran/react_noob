f8e2241e7165068d99a0eeda1793b330d14f2767
amitbiran <amitbiran2@gmail.com>
2021-06-21 14:08:11 +0300

    ran loops inside a component to dynamically generate content

--- src/App.js
+++ src/App.js
@@ -1,35 +1,50 @@
 import "./styles.css";
 
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s Kitchen</h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>I Am Groot!!!!</p>
+      <p>
+        I Am Groot!!!! Groot Is {props.adjective} Groot has a resturans. we
+        serve:
+      </p>
+      <ul style={{ textAlign: "Left" }}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
     </section>
   );
 }
 
-function Footer() {
+function Footer(props) {
   return (
     <footer>
-      <p>He Really Is Groot</p>
+      <p>He Really Is Groot! and the year is {props.year} in groot years!</p>
     </footer>
   );
 }
 
+const dishes = ["pasta", "tofu", "chicken tika masala"];
+const dishesObjects = dishes.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
 export default function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header name="Amit" />
+      <Main adjective="amazing" dishes={dishesObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }

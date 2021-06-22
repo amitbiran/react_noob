f6ff8225daf89b8973c040d847d3cf8c8d78ce25
amitbiran <amitbiran2@gmail.com>
2021-06-22 14:35:06 +0300

    finished async chapter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import { useReducer } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
-export default function App() {
-  const [checked, toggle] = useReducer((checked) => !checked, false);
-  //reducer handles changes in the state, its a function that gets a state and return a diffenet state
+//https://api.github.com/users/amitbiran
+
+export default function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (!login) return;
+
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((resp) => resp.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+  if (!data) return null;
 
   return (
-    <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </>
+    <div>
+      <h1>{data.login}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} height="200" />
+    </div>
   );
 }
